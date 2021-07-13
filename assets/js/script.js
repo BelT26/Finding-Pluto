b3b72bde05ad7fccf49ca0dcddcd0bf6761e12ba
BelT26 <helen.taylor@hotmail.it>
2021-07-13 13:35:19 +0000

    create js script and link to html.  Select html elements with js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+//html elements in fuel gauge
+const gauge = document.getElementById('gauge');
+const fuelGauge = document.getElementById('fuel-in-gauge');
+const fuelNumber = document.getElementById("fuel-number");
+
+//html elements in quiz container
+const questionImage = document.getElementById('question-image');
+const questionText = document.getElementById('question-text');
+const answerA = document.getElementById('A');
+const answerB = document.getElementById('B');
+const answerC = document.getElementById('C');
+
+
+//html elements in planet stats
+const planetImg = document.getElementById('planet-img'); 
+const planetName = document.getElementById('planet-name');
+const size = document.getElementById('size');
+const year = document.getElementById('year');
+const day = document.getElementById('day');
+const distance = document.getElementById('distance');
+const moons = document.getElementById('moons');
+
+//html elements for flight path
+const flightContainer = document.getElementById('flight-container');
+
