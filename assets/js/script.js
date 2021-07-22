e00608ffe976e534439a92e02b3bb96ebf35aa2d
BelT26 <helen.taylor@hotmail.it>
2021-07-22 13:50:43 +0000

    remove test buttons for fuel gauge functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,6 @@
 const gauge = document.getElementById('gauge');
 const fuelGauge = document.getElementById('fuel-in-gauge');
 const fuelNumber = document.getElementById("fuel-number");
-const addFuelBtn = document.getElementById('add-fuel');
-const loseFuelBtn = document.getElementById('lose-fuel');
-const resetFuelBtn = document.getElementById('reset-fuel');
 
 //html elements in quiz container
 const questionImage = document.getElementById('question-image');
@@ -321,9 +318,6 @@ function loseFuel() {
     }
 }
 
-addFuelBtn.addEventListener('click', addFuel);
-loseFuelBtn.addEventListener('click', loseFuel);
-resetFuelBtn.addEventListener('click', resetFuel);
 
 //variable to keep track of current stats to display
 let currentStatsIndex = 0;
